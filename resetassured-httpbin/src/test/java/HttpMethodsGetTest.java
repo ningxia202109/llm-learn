6a2a9cad740adb57b3e0f8dbe411a978d3e48763
ningxia202109 <90520833+ningxia202109@users.noreply.github.com>
2024-11-22 01:00:13 +0000

    add resetassured-httpbin

---
+++ resetassured-httpbin/src/test/java/HttpMethodsGetTest.java
@@ -0,0 +1,45 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class HttpMethodsGetTest {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://httpbin.org";
+    }
+
+    @Test
+    public void testGet() {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/get");
+
+        // Log the response body with class and method name
+        String className = this.getClass().getSimpleName();
+        String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+        System.out.println("Response Body for " + className + "." + methodName + ":");
+        System.out.println(response.getBody().asPrettyString());
+
+        // Perform assertions
+        response.then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("url", equalTo("https://httpbin.org/get"))
+                .body("$", hasKey("args"))
+                // .body("headers.Accept", equalTo("application/json"))
+                // .body("headers.Host", equalTo("httpbin.org"))
+                .body("origin", not(emptyOrNullString()))
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Content-Type", "application/json")
+                .header("Server", "gunicorn/19.9.0");
+    }
+}
